476eee6c64f8ce2b748af87ea6c365bafa7c2c6f
Escrutador <deportelecine27@gmail.com>
2026-01-26 23:55:40 +0100

    Delete proxy-quiniela.js

--- proxy-quiniela.js
+++
@@ -1,8 +0,0 @@
-fetch("https://www.loteriasyapuestas.es/servicios/xml/resultados/quiniela.xml")
-  .then(r => r.text())
-  .then(xml => {
-    document.write(xml);
-  })
-  .catch(() => {
-    document.write("<error>No disponible</error>");
-  });
